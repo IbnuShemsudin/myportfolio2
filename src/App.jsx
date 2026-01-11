8daad71e13fd2927cae6e4fb6d226f923895cf70
Ibnushemsudin <musteshemsu@gmail.com>
2026-01-11 04:52:46 -0800

    implement Darg mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react"
+import Aos from "aos"
+import "aos/dist/aos.css"
+import Navbar from "./components/Navbar"
+
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(true)
+  useEffect(() => {
+    Aos.init({ 
+      duration: 2000,
+      once: false,
+      offset: 100,
+    })
+    document.documentElement.classList.add('dark');
+  }, [])
+   
+  const toggleDarkMode = () => {
+    const newMode = !darkMode;
+    setDarkMode(!darkMode);
+    document.documentElement.classList.toggle('dark', newMode);
+  }
+  
+
+
+
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className={
+      darkMode ? "dark bg-gray-900 text-white min-h-screen transition-colors duration-500"
+      : "bg-white text-black min-h-screen transition-colors duration-500"
+    }>
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
     </div>
   )
 }
